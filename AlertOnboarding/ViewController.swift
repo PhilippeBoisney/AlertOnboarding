b0d2179d0353d8697e340287586f71eda6517d9e
webdevotion <webdevotion>
2017-07-06 12:57:55 +0200

    gitignore.io generated settings for xcode, cocoapods and xcode

--- AlertOnboarding/ViewController.swift
+++ AlertOnboarding/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController, AlertOnboardingDelegate {
     
     var alertView: AlertOnboarding!
     
-    var arrayOfImage = ["onboarding", "train", "onboarding"]
+    var arrayOfImage = ["onboarding", "train", "train"]
     var arrayOfTitle = ["CREATE ACCOUNT", "CHOOSE THE PLANET\nACROSS TWO LINES", "DEPARTURE"]
     var arrayOfDescription = ["In your profile, you can view the statistics of its operations and the recommandations of friends",
                               "Purchase tickets on hot tours to your favorite planet and fly to the most comfortable intergalactic spaceships of best companies. Purchase tickets on hot tours to your favorite planet and fly to the most comfortable intergalactic spaceships of best companies.",
@@ -28,13 +28,13 @@ class ViewController: UIViewController, AlertOnboardingDelegate {
       // IF YOU WANT TO CUSTOMISE ALERTVIEW
       self.alertView.colorForAlertViewBackground = .darkGray
       self.alertView.colorButtonText = .white
-      self.alertView.colorButtonBottomBackground = .blue
+      self.alertView.colorButtonBottomBackground = .lightGray
 
       self.alertView.colorTitleLabel = .white
       self.alertView.colorDescriptionLabel = .white
 
       self.alertView.colorPageIndicator = .white
-      self.alertView.colorCurrentPageIndicator = .blue
+      self.alertView.colorCurrentPageIndicator = .lightGray
 
       self.alertView.percentageRatioHeight = 1.0
       self.alertView.percentageRatioWidth = 1.0
