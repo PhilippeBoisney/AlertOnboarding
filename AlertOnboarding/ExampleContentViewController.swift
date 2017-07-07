ece29d1fdc96b905cd42ec00495adcbb3d1e787f
webdevotion <webdevotion>
2017-07-07 14:08:32 +0200

    installed reveal-sdk as cocoapods

---
+++ AlertOnboarding/ExampleContentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ExampleContentViewController.swift
+//  AlertOnboarding
+//
+//  Created by Bram Plessers on 07/07/2017.
+//  Copyright © 2017 CookMinute. All rights reserved.
+//
+
+import UIKit
+
+class ExampleContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'AlertOnboarding' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for AlertOnboarding
+  pod 'Reveal-SDK', :configurations => ['Debug']
+end
